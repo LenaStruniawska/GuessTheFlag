ba634a5c38a77da43b8c4196edffa1a3d04e1023
Magdalena Struniawska <mag.sok@onet.pl>
2022-02-28 14:03:57 +0100

    done GuessTheFlag

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,23 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAlert = false
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var countries = ["Nigeria", "Poland", "Estonia", "Germany", "Ireland", "Italy", "France", "Russia", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
-        Button("Show Alert") {
-            showingAlert = true
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.red, .blue]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            VStack (spacing: 30) {
+                VStack {
+                    Text("Guess the flag:")
+                        .foregroundColor(.white)
+                        .font(.subheadline.weight(.heavy))
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle.weight(.semibold))
+                }
+                ForEach(0..<3) { number in
+                    Button {
+                        flagTapped(number)
+                    }label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                            .shadow(radius: 10)
+                            .clipShape(Capsule())
+                    }
+                    
+                }
+            }
         }
-        .alert("Important message",isPresented: $showingAlert) {
-            Button("Delete", role: .destructive) { }
-            Button("Cancel", role: .cancel) { }
-        } message : {
-            Text("Please read this")
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text("Your score is?")
         }
     }
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        showingScore = true
+    }
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+    
+
